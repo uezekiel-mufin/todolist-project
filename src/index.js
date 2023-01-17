8d39eba0462d5145e859475ce04d6a04d7a70b29
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-01-17 15:11:17 +0100

    implemented review changes

--- src/index.js
+++ src/index.js
@@ -30,27 +30,35 @@ const todoLists = [
   },
 ];
 
-todoLists.forEach((item) => {
-  const itemContainer = document.createElement('li');
-  itemContainer.className = 'todo';
-  const todoTitle = document.createElement('h3');
-  todoTitle.className = 'todo_title';
-  todoTitle.innerText = item.description;
-  const checkBox = document.createElement('input');
-  checkBox.type = 'checkbox';
-  checkBox.checked = item.completed;
-  checkBox.className = 'checkbox';
-  const option = document.createElement('span');
-  option.className = 'material-symbols-outlined';
-  option.innerText = 'more_vert';
+const displayTodos = () => {
+  for (let i = 0; i < todoLists.length; i += 1) {
+    const item = todoLists[i];
+    const itemContainer = document.createElement('li');
+    itemContainer.className = 'todo';
+    const todoTitle = document.createElement('h3');
+    todoTitle.className = 'todo_title';
+    todoTitle.innerText = item.description;
+    const checkBox = document.createElement('input');
+    checkBox.type = 'checkbox';
+    checkBox.checked = item.completed;
+    checkBox.className = 'checkbox';
+    const option = document.createElement('span');
+    option.className = 'material-symbols-outlined';
+    option.innerText = 'more_vert';
 
-  const div = document.createElement('div');
-  div.appendChild(checkBox);
-  div.appendChild(todoTitle);
-  div.className = 'todo_right';
+    const div = document.createElement('div');
+    div.appendChild(checkBox);
+    div.appendChild(todoTitle);
+    div.className = 'todo_right';
 
-  itemContainer.appendChild(div);
-  itemContainer.appendChild(option);
+    itemContainer.appendChild(div);
+    itemContainer.appendChild(option);
 
-  todoUl.appendChild(itemContainer);
+    todoUl.appendChild(itemContainer);
+  }
+};
+
+window.addEventListener('load', () => {
+  displayTodos();
+  return false;
 });
